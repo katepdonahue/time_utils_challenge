f2cde30ca8d2489d85e737a1350e7d4ecf22de6f
Kate <katepdonahue@gmail.com>
2013-12-05 12:44:29 -0500

    Working for times in the same half of the day without considering am pm at all

--- spec.rb
+++ spec.rb
@@ -4,8 +4,8 @@ describe Array do
   
   describe "#average_time_of_day" do
     it "should give correct average when times are of different hours and days" do
-      time_array = ["1:51pm", "1:56pm", "2:01am", "2:06am", "2:11am"]
-      expect(time_array.average_time_of_day).to eq("2:01am")
+      time_array = ["11:51pm", "11:56pm", "12:01am", "12:06am", "12:11am"]
+      expect(time_array.average_time_of_day).to eq("12:01am")
     end
     # it "should give correct average when times are around the switch to 1:00" do
     #   time_array = ["12:51pm", "12:56pm", "1:01pm", "1:06pm", "1:11pm"]
